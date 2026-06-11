8a9ff93ac065d58a275b93ee7644bcc51bf2bb23
lavapit <ap7901@srmist.edu.in>
2026-06-11 10:48:31 +0530

    Add: find-the-town-judge - submission-2

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> incoming(n+1,0);
+        vector<int> outgoing(n+1,0);
+        for (int i = 0; i < trust.size(); i++)
+        {
+            outgoing[trust[i][0]]++;
+            incoming[trust[i][1]]++;
+        }
+        for(int i=0;i<=n;i++){
+            if(outgoing[i]==0 && incoming[i]==n-1){
+                return i;
+            }
+            
+                
+            
+        }
+        return -1;
+    }
+};
